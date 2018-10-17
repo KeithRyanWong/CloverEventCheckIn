74983224cdf890cacc29cc57c383e4254013363b
Keith Wong <keithryanwong@gmail.com>
2018-10-17 10:38:29 -0700

    Format names and email

--- QRCodeGeneration/generator.py
+++ QRCodeGeneration/generator.py
@@ -1,8 +1,19 @@
+# Take in infoirmation about csv email list, merchantID, etc.
+# Get OAuth Token
 # Read First names, Last names and emails into a dictionary
-# Go through the list and generate QR codes, appending them to dictionary entries
+# Go through the list to create Customers based on CSV and generate QR codes, appending IDs and codes to dictionary entries
 # Write dictionary to CSV
 import csv
 import qrcode
+import http
+
+
+def normalize(name):
+    name = name.strip()
+    nameParts = name.split(' ')
+    for idx in range(len(nameParts)): 
+        nameParts[idx] = nameParts[idx].lower().capitalize()
+    return ' '.join(nameParts)
 
 def parseContactList(list):
     parsedList = []
@@ -14,6 +25,9 @@ def parseContactList(list):
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
+            row["First Name"] = normalize(row["First Name"])
+            row["Last Name"] = normalize(row["Last Name"])
+            row["Email"] = row["Email"].strip()
             parsedList.append(row)
         print(f'Processed {line_count} lines.')    
 
@@ -23,15 +37,16 @@ def generateQRCode(content):
     img = qrcode.make(content)
     return img
 
+#Change to encode for customer ID
 def encodeEmails(list):
     for attendee in list:
         qrcode = generateQRCode(attendee["Email"])
         qrcode.save(f'./codes/{attendee["First Name"]}QRCode', 'png')
-        attendee["QR Code"] = f'{attendee["First Name"]}QRCode.png'
+        attendee["QR Code"] = f'{attendee["First Name"]}{attendee["Last Name"]}QRCode.png'
 
 attendees = parseContactList('Test Email List.csv')
 
-encodeEmails(attendees)
+# encodeEmails(attendees)
 
 for attendee in attendees:
     print(f'\t{attendee["First Name"]} {attendee["Last Name"]}\'s email address is {attendee["Email"]}.')
