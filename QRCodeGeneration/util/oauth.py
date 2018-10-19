e3227b09599434d077087bc131d7f5eb0d478302
Keith Wong <keithryanwong@gmail.com>
2018-10-19 11:15:29 -0700

    Automate merchant login

--- QRCodeGeneration/util/oauth.py
+++ QRCodeGeneration/util/oauth.py
@@ -7,26 +7,21 @@ import time
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 
-sandbox_host = 'sandbox.dev.clover.com'
-prod_host = 'clover.com'
-app_id = 'B8KAN7XASTP4M'
-app_secret = ''
-merchant_email = "crocinusluxfiat@gmail.com"
-merchant_pwd = "Letmein!"
-merchant_name = "Kwover2"
 
-def getAuthCode():
+
+
+def getAuthCode(merchant, email, pwd):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--incognito')
     driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
     driver.set_page_load_timeout(30)
     driver.implicitly_wait(10)
     driver.get(f'https://sandbox.dev.clover.com/oauth/authorize?client_id={app_id}')
-    driver.find_element_by_id("email").send_keys(merchant_email)
-    driver.find_element_by_id("password").send_keys(merchant_pwd)
+    driver.find_element_by_id("email").send_keys(email)
+    driver.find_element_by_id("password").send_keys(pwd)
     driver.find_element_by_id("login-submit").click()
     if(driver.current_url == f'https://sandbox.dev.clover.com/oauth/authorize?client_id={app_id}'):
-        driver.find_element_by_xpath("//a[text()='Kwover2']").click()
+        driver.find_element_by_xpath(f'//a[text()={merchant}]').click()
 
     time.sleep(2)
     authURL = driver.current_url
@@ -38,9 +33,9 @@ def getAuthCode():
     mid = qs["merchant_id"][0]
 
     print(auth_code)
+    driver.quit()
 
     return auth_code
 
 
 
-driver.quit()
