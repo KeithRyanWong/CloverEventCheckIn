4335e26edbe2e2c0d9df6d9b5a6b0b0e8074368f
Keith Wong <keithryanwong@gmail.com>
2018-11-05 15:37:45 -0800

    Move views under a parent layout

--- CheckInApp/app/src/main/java/com/kwover/eventcheck_in/Customers.java
+++
@@ -1,8 +0,0 @@
-package com.kwover.eventcheck_in;
-
-/**
- * Created by keithwong on 10/25/18.
- */
-
-public class Customers {
-}
--- CheckInApp/app/src/main/java/com/kwover/eventcheck_in/StartupActivity.java
+++ CheckInApp/app/src/main/java/com/kwover/eventcheck_in/StartupActivity.java
@@ -7,19 +7,20 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+//import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.clover.sdk.v1.Intents;
-import com.clover.sdk.v1.customer.Customer;
 import com.clover.sdk.v3.scanner.BarcodeScanner;
 import com.kwover.eventcheck_in.util.ActivityCallbackInterface;
 import com.kwover.eventcheck_in.util.Customers;
+
+
 
 public class StartupActivity extends AppCompatActivity {
     protected Customers customers;
@@ -29,13 +30,22 @@ public class StartupActivity extends AppCompatActivity {
 
     private static final String TAG = "Startup Activity";
 
-    private ProgressBar loadBar;
-    private TextView loadMsg;
-    private TextView tip1Txt;
+//    private ProgressBar loadBar;
+//    private TextView loadMsg;
+//    private TextView tip1Txt;
     private TextView warningMsg;
     private TextView welcomeMsg;
     private Button startScanBtn;
     private Button acknowledgeErrorBtn;
+    private ConstraintLayout loadingView;
+    private ConstraintLayout mainView;
+    private ConstraintLayout errorView;
+    private ConstraintLayout welcomeView;
+    private static final String LOADING_VIEW = "LOADING_VIEW";
+    private static final String MAIN_VIEW = "MAIN_VIEW";
+    private static final String ERROR_VIEW = "ERROR_VIEW";
+    private static final String WELCOME_VIEW = "WELCOME_VIEW";
+
 
 
 //    When app is started:
@@ -51,15 +61,24 @@ public class StartupActivity extends AppCompatActivity {
         
         
         setContentView(R.layout.activity_startup);
-        loadBar = (ProgressBar) findViewById(R.id.loading_bar);
-        loadMsg = (TextView) findViewById(R.id.loading_message);
-        tip1Txt = (TextView) findViewById(R.id.tip1);
-        startScanBtn = (Button) findViewById(R.id.startScan);
-        acknowledgeErrorBtn = (Button) findViewById(R.id.acknowledgementBtn);
-        warningMsg = (TextView) findViewById(R.id.warningMsg);
-        welcomeMsg = (TextView) findViewById(R.id.welcomeMsg);
+//        loadBar = (ProgressBar) findViewById(R.id.loading_bar);
+//        loadMsg = (TextView) findViewById(R.id.loading_message);
+//        tip1Txt = (TextView) findViewById(R.id.tip1);
+        startScanBtn = findViewById(R.id.startScan);
+        acknowledgeErrorBtn = findViewById(R.id.acknowledgementBtn);
+        warningMsg = findViewById(R.id.warningMsg);
+        welcomeMsg = findViewById(R.id.welcomeMsg);
+        loadingView = findViewById(R.id.loadingView);
+        mainView = findViewById(R.id.mainView);
+        errorView = findViewById(R.id.errorView);
+        welcomeView = findViewById(R.id.welcomeView);
+
+        transitionToLoad();
+
+
 
         customers = new Customers();
+        final Context context = this;
 
         startScanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,8 +93,6 @@ public class StartupActivity extends AppCompatActivity {
                 transitionToMain();
             }
         });
-
-        final Context context = this;
 
         customers.openDb(context);
         customers.syncDb(context, new ActivityCallbackInterface() {
@@ -104,8 +121,6 @@ public class StartupActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart: RP)*#H(*RH@#( %(Y@#(*Y%(H@$IURHTKUEWHFIEGHRF(H Q(*(*#@Y RIHKUEFHIUPQGF*IGQIEWGFI QWG(F(*@#(Y@*5F4W68R460Q @W3210@6!54 R6840Q@#6540R 86#Q4R60QW");
-        
     }
 
     @Override
@@ -116,58 +131,19 @@ public class StartupActivity extends AppCompatActivity {
         barcodeReceiver = new BarcodeReceiver();
 
         registerReceiver(barcodeReceiver, new IntentFilter(BARCODE_BROADCAST));
-
-//        startScanBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startScanForQR();
-//            }
-//        });
-//
-//        acknowledgeErrorBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                transitionToMain();
-//            }
-//        });
-
-
-
-//        final Context context = this;
-//
-//        customers.openDb(context);
-//        customers.syncDb(context, new ActivityCallbackInterface() {
-//                    @Override
-//                    public void onSyncFinishOk() {
-//                        //Remove loading bar and message
-////                        ProgressBar load_bar = (ProgressBar) findViewById(R.id.loading_bar);
-////                        TextView load_msg = (TextView) findViewById(R.id.loading_message);
-////
-////                        load_bar.setVisibility(View.INVISIBLE);
-////                        load_msg.setVisibility(View.INVISIBLE);
-//                        //transition to check in activity
-//                        transitionToMain();
-////                        startScanForQR();
-//                    }
-//
-//                    @Override
-//                    public void onSyncFinishBad() {
-//                        //transition to error message
-//                        Log.e(TAG, "onSyncFinishBad: There was an error grabbing data from the server");
-//                        transitionToError("Se encontró un error al intentar conectarse al servidor. Procediendo sin conexión");
-//                    }
-//
-//                    @Override
-//                    public void onUpdateFinished(Boolean finishedOk, String[] customerName) {
-//
-//                    }
-//                }
-//        );
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+//        Log.i(TAG, "onPause: Closing DB and unregistering broadcast receiver.");
+//        customers.closeConnection();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         Log.i(TAG, "onPause: Closing DB and unregistering broadcast receiver.");
         customers.closeConnection();
         unregisterReceiver(barcodeReceiver);
@@ -181,6 +157,7 @@ public class StartupActivity extends AppCompatActivity {
     private void startScanForQR() {
         //start the scanner and set a timeout in case nothing gets scanned
         startBarcodeScanner();
+
     }
 
     private void markCustomerAttended(final String customerId) {
@@ -189,12 +166,12 @@ public class StartupActivity extends AppCompatActivity {
         customers.markCustomerAttended(context, customerId , new ActivityCallbackInterface() {
             @Override
             public void onSyncFinishOk() {
-                return;
+
             }
 
             @Override
             public void onSyncFinishBad() {
-                return;
+
             }
 
             @Override
@@ -206,54 +183,46 @@ public class StartupActivity extends AppCompatActivity {
                 } else {
                     Log.i(TAG, "onUpdateFinished: Error syncing " + customerId + " through Customers Service.");
                 }
-                return;
+
             }
         });
         
     }
 
+    private void toggleVisibleView(String view) {
+        loadingView.setVisibility(view == LOADING_VIEW ? View.VISIBLE : View.GONE);
+        mainView.setVisibility(view == MAIN_VIEW ? View.VISIBLE : View.GONE);
+        errorView.setVisibility(view == ERROR_VIEW ? View.VISIBLE : View.GONE);
+        welcomeView.setVisibility(view == WELCOME_VIEW ? View.VISIBLE : View.GONE);
+    }
+
+    private void transitionToLoad() {
+        toggleVisibleView(LOADING_VIEW);
+    }
+
     private void transitionToMain() {
-        loadBar.setVisibility(View.GONE);
-        loadMsg.setVisibility(View.GONE);
-        welcomeMsg.setVisibility(View.GONE);
-
-        acknowledgeErrorBtn.setVisibility(View.GONE);
-        warningMsg.setVisibility(View.GONE);
-
-        //set button to start scan visible
-        tip1Txt.setVisibility(View.VISIBLE);
-        startScanBtn.setVisibility(View.VISIBLE);
-
-//        transitionToCheckedIn(new String[]{"KEITH", "WONG"});
+        toggleVisibleView(MAIN_VIEW);
     }
 
     private void transitionToError(String errMsg) {
-        loadBar.setVisibility(View.GONE);
-        loadMsg.setVisibility(View.GONE);
-        tip1Txt.setVisibility(View.GONE);
-        startScanBtn.setVisibility(View.GONE);
-        welcomeMsg.setVisibility(View.GONE);
-
-        //set button to start transition to main screen
-        acknowledgeErrorBtn.setVisibility(View.VISIBLE);
         warningMsg.setText(errMsg);
-        warningMsg.setVisibility(View.VISIBLE);
+        toggleVisibleView(ERROR_VIEW);
     }
 
     private void transitionToCheckedIn(String[] customerName) {
-        loadBar.setVisibility(View.GONE);
-        loadMsg.setVisibility(View.GONE);
-        tip1Txt.setVisibility(View.GONE);
-        startScanBtn.setVisibility(View.GONE);
-        acknowledgeErrorBtn.setVisibility(View.GONE);
-        warningMsg.setVisibility(View.GONE);
+//        loadBar.setVisibility(View.GONE);
+//        loadMsg.setVisibility(View.GONE);
+//        tip1Txt.setVisibility(View.GONE);
+//        startScanBtn.setVisibility(View.GONE);
+//        acknowledgeErrorBtn.setVisibility(View.GONE);
+//        warningMsg.setVisibility(View.GONE);
 
         String msg = "El huésped ha sido registrado. Por favor salude al " +
                 customerName[0] + " " +
                 customerName[1] + ".";
 
+        toggleVisibleView(WELCOME_VIEW);
         welcomeMsg.setText(msg);
-        welcomeMsg.setVisibility(View.VISIBLE);
 
         new CountDownTimer(5000, 1000) {
             @Override
@@ -279,21 +248,6 @@ public class StartupActivity extends AppCompatActivity {
 
     public boolean startBarcodeScanner() {
         return new BarcodeScanner(this).startScan(getBarcodeSetting(true));
-
-//        try {
-//            Thread.sleep(5000);
-//            if (new BarcodeScanner(this).isProcessing()) {
-//                stopBarcodeScanner();
-//                Log.i(TAG, "startBarcodeScanner: Please start scanner again if nothing has happened");
-//            } else {
-//                Log.i(TAG, "startBarcodeScanner: Scan completed already");
-//            }
-//        } catch (Exception e) {
-//            Log.e(TAG, "startBarcodeScanner: unknown error prevented barcode scanner from closing");
-//            return false;
-//        }
-
-//        return true;
     }
 
     public boolean stopBarcodeScanner() {
